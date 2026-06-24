5ea7547ca4b315f39eeec93f1e9dcdd108894c99
Nguyen Hoang Nam <namnh98@gmail.com>
2026-06-24 14:32:57 +0700

    fix: code error

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './', // hoặc '/' tùy deploy
+  base: "./",
   build: {
-    outDir: 'dist'
-  }
-})
+    outDir: "dist",
+  },
+});
