80618a0d8e88884028a31c6468eb4bd495ba100c
Nguyen Hoang Nam <namnh98@gmail.com>
2026-06-24 14:32:39 +0700

    fix; code error

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-const repoName =
-  process.env.GITHUB_REPOSITORY?.split("/").pop() ||
-  "evd-file-management-web-app";
-
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? `/${repoName}/` : "/",
-  plugins: [react()],
-});
+  base: './', // hoặc '/' tùy deploy
+  build: {
+    outDir: 'dist'
+  }
+})
